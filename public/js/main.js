6fdb74056fb0c21bb292268d34a69ef5ca3bc161
mupezzuol <murillopezzuol@hotmail.com>
2019-05-28 00:20:59 -0300

    routeProvider, locationProvider. Rotas; html5Mode; Ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 //Criando módulo principal do angular
 //Adicionamos uma depência desse módulo, que um outro módulo chamado 'minhasDiretivas'
-angular.module('angularpic', ['minhasDiretivas','ngAnimate']);
+angular.module('angularpic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {
+        
+        $locationProvider.html5Mode(true);
+        
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotosController'
+        });
+
+        $routeProvider.when('/fotos/new', {
+            templateUrl: 'partials/foto.html'
+        });
+
+        $routeProvider.otherwise({redirectTo: '/fotos'});
+
+    });
